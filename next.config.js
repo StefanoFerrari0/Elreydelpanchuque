061ceb3fbe6c887176567e730cf1d3407b68dad6
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2021-08-14 14:03:53 -0300

    error 3

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     domains: ['res.cloudinary.com'],
   },
   env: {
-    BASE_URL: "https://el-rey-del-panchuque.vercel.app/",
+    BASE_URL: "http://localhost:3000",
     MONGODB_URL: "mongodb+srv://stefano:123@cluster0.vcetv.mongodb.net/Ecommerce?retryWrites=true&w=majority",
     TOKEN_SECRET: "-{<q?mNJ-dq9]F&&+`k^+Y#:3c[[cE+ApS-[JB@]u5VTan~H",
     REFRESH_TOKEN: "8v'Z.NL7A$w~'t:NjEj)(Ck:Snpj2{6f@d~NsF^;r4B}R}R->s"
